7e1c83bdd96698a901f91a996d570ff6a25e5cf8
emredindr <emredindr@gmail.com>
2022-06-14 07:56:51 +0300

    Home servis arayüzleri oluşturuldu.

---
+++ eShopOnContainers.Core/Services/MyCategory/ICategoryManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using eShopOnContainers.Core.Models.MyCategory;
+
+namespace eShopOnContainers.Core.Services.MyCategory
+{
+    public interface ICategoryManager
+    {
+        List<MainCategory> GetMainCategories();
+        List<SubCategory> GetSubCategories();
+        List<SubOfSubCategory> GetSubOfSubCategories();
+        List<SubCategory> GetSubCategories(MainCategory mainCategory);
+        List<SubOfSubCategory> GetSubOfSubCategories(SubCategory subCategory);
+        List<SubCategory> GetElektronicPopulerCategories();
+        List<SubCategory> GetBabyPopulerCategories();
+    }
+}
