8bd6b18107b3c74b5e96a21e6e3dc7a22653b5be
emredindr <emredindr@gmail.com>
2022-06-14 08:25:08 +0300

    Account servis için concretes nesnesi oluşturuldu ve servisler bağlandı.

---
+++ eShopOnContainers.Core/Services/MyAccount/AccountManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using eShopOnContainers.Core.Models.MyAccount;
+using eShopOnContainers.Core.Services.MyRestService;
+
+namespace eShopOnContainers.Core.Services.MyAccount
+{
+    public class AccountManager : IAccountManager
+    {
+        private readonly RestServiceManager<List<AccountPageItem>> _restServiceAccountPageItemManager;
+        private readonly RestServiceManager<List<AccountPageItemLogined>> _restServiceAccountPageItemLoginedManager;
+        public AccountManager()
+        {
+            _restServiceAccountPageItemLoginedManager = new RestServiceManager<List<AccountPageItemLogined>>();
+            _restServiceAccountPageItemManager = new RestServiceManager<List<AccountPageItem>>();
+        }
+        public List<AccountPageItem> GetAccountPageItems()
+        {
+
+            return _restServiceAccountPageItemManager.GetServiceResponse(ServiceUrlConst.AccountPageItem);
+
+        }
+
+        public List<AccountPageItemLogined> GetAccountPageItemsLogined()
+        {
+
+            return _restServiceAccountPageItemLoginedManager.GetServiceResponse(ServiceUrlConst.AccountPageItemLogined);
+        }
+    }
+}
