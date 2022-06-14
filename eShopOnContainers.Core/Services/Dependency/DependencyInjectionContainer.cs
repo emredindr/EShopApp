0565aba0da02378b4734efa84651f46c29bb9482
emredindr <emredindr@gmail.com>
2022-06-14 10:44:24 +0300

    Dependency Injection Oluşturuldu.

---
+++ eShopOnContainers.Core/Services/Dependency/DependencyInjectionContainer.cs
@@ -0,0 +1,33 @@
+﻿using eShopOnContainers.Core.MyViewModels;
+using eShopOnContainers.Core.Services.MyAccount;
+using eShopOnContainers.Core.Services.MyCategory;
+using eShopOnContainers.Core.Services.MyHome;
+using eShopOnContainers.Core.Services.MyProduct;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eShopOnContainers.Core.Services.Dependency
+{
+    public static class DependencyInjectionContainer
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAccountManager, AccountManager>();
+            services.AddScoped<IHomeManager, HomeManager>();
+            services.AddScoped<ICategoryManager, CategoryManager>();
+            services.AddScoped<IProductManager, ProductManager>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureViewModels(this IServiceCollection services)
+        {
+            services.AddTransient<Home>();
+            services.AddTransient<Category>();
+            services.AddTransient<Account>();
+            services.AddTransient<Product>();
+
+
+            return services;
+        }
+    }
+}
