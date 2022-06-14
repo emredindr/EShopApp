5c2156db4c6214e315f4074276c9a600cc72a7c7
emredindr <emredindr@gmail.com>
2022-06-14 08:04:25 +0300

    Basket servis arayüzleri oluşturuldu.

---
+++ eShopOnContainers.Core/Services/MyBasket/IMyBasketManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using eShopOnContainers.Core.Models.MyBasket;
+
+namespace eShopOnContainers.Core.Services.MyBasket
+{
+    public interface IMyBasketManager
+    {
+        MyBasketItem Get(int Id);
+        int GetAllCount();
+        IEnumerable<MyBasketItem> GetAll();
+        int Insert(MyBasketItem s);
+        int Update(MyBasketItem s);
+        int Delete(int Id);
+
+        void Dispose();
+    }
+}
