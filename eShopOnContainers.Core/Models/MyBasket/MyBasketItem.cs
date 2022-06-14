5c5177acc1bb054e675e685f9744c5e91ac876f9
emredindr <emredindr@gmail.com>
2022-06-14 07:41:01 +0300

    Basket modelleri oluşturuldu.

---
+++ eShopOnContainers.Core/Models/MyBasket/MyBasketItem.cs
@@ -0,0 +1,15 @@
+﻿using eShopOnContainers.Core.Models.Common;
+
+namespace eShopOnContainers.Core.Models.MyBasket
+{
+    public class MyBasketItem : BaseEntity
+    {
+        public int ProductId { get; set; }
+        public string ProductTitle { get; set; }
+        public int MainCategoryId { get; set; }
+        public int SubCategoryId { get; set; }
+        public int SubOfSubCategoryId { get; set; }
+        public int ProductCount { get; set; }
+        public float ProductPrice { get; set; }
+    }
+}
