be9723461b697c90bbbc730067787e2d234a195c
emredindr <emredindr@gmail.com>
2022-06-14 07:56:43 +0300

    Account servis arayüzleri oluşturuldu.

---
+++ eShopOnContainers.Core/Services/MyAccount/IAccountManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using eShopOnContainers.Core.Models.MyAccount;
+
+namespace eShopOnContainers.Core.Services.MyAccount
+{
+    public interface IAccountManager
+    {
+        List<AccountPageItem> GetAccountPageItems();
+        List<AccountPageItemLogined> GetAccountPageItemsLogined();
+    }
+}
