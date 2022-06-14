23e3ea37618a8576dd7a0ed5924fed59a797df9f
emredindr <emredindr@gmail.com>
2022-06-14 08:25:33 +0300

    Home servis için concretes nesnesi oluşturuldu ve servisler bağlandı.

---
+++ eShopOnContainers.Core/Services/MyHome/HomeManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using eShopOnContainers.Core.Models.MyHome;
+using eShopOnContainers.Core.Services.MyRestService;
+
+namespace eShopOnContainers.Core.Services.MyHome
+{
+    public class HomeManager : IHomeManager
+    {
+        private readonly RestServiceManager<List<Banner>> _restServiceBannerManager;
+        public HomeManager()
+        {
+            _restServiceBannerManager = new RestServiceManager<List<Banner>>();
+        }
+        public List<Banner> GetBanners()
+        {
+            return _restServiceBannerManager.GetServiceResponse(ServiceUrlConst.Banner);
+        }
+    }
+}
