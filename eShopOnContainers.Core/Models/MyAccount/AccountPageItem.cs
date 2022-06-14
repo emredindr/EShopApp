68ed554cd34a18732e5e57f47df1a67bc9f0f51b
emredindr <emredindr@gmail.com>
2022-06-14 07:40:06 +0300

    Account modelleri oluşturuldu.

---
+++ eShopOnContainers.Core/Models/MyAccount/AccountPageItem.cs
@@ -0,0 +1,10 @@
+﻿using eShopOnContainers.Core.Models.Common;
+
+namespace eShopOnContainers.Core.Models.MyAccount
+{
+    public class AccountPageItem:BaseEntity
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ eShopOnContainers.Core/Models/MyAccount/AccountPageItemLogined.cs
@@ -0,0 +1,10 @@
+﻿using eShopOnContainers.Core.Models.Common;
+
+namespace eShopOnContainers.Core.Models.MyAccount
+{
+    public class AccountPageItemLogined : BaseEntity
+    {
+        public string Title { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
