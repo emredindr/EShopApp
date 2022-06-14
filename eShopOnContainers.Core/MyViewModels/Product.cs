962d680c5c399ac7d57c145bf5c1aa7abe9e8041
emredindr <emredindr@gmail.com>
2022-06-14 10:43:17 +0300

    Product viewmodel oluşturuldu.

---
+++ eShopOnContainers.Core/MyViewModels/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using eShopOnContainers.Core.Services.MyProduct;
+
+namespace eShopOnContainers.Core.MyViewModels
+{
+    public class Product
+    {
+        public List<Models.MyProduct.Product> Products { get; set; }
+        public Product(IProductManager productManager)
+        {
+            Products = productManager.GetProducts();
+        }
+    }
+}
