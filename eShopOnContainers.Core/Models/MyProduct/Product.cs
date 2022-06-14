2746d1f74eea899980d28a8c9cc8d42573f5c6a9
emredindr <emredindr@gmail.com>
2022-06-14 07:41:30 +0300

    Product modelleri oluşturuldu.

---
+++ eShopOnContainers.Core/Models/MyProduct/Product.cs
@@ -0,0 +1,16 @@
+﻿using eShopOnContainers.Core.Models.Common;
+
+namespace eShopOnContainers.Core.Models.MyProduct
+{
+    public class Product : BaseEntity
+    {
+        public int CategoryId { get; set; }
+        public int SubCategoryId { get; set; }
+        public int SubOfSubCategoryId { get; set; }
+        public string ProductIconSource { get; set; }
+
+        public string Title { get; set; }
+        public float Price { get; set; }
+        public string PriceWithTL => (Price.ToString() + ".00 TL");
+    }
+}
---
+++ eShopOnContainers.Core/Models/MyProduct/ProductFilter.cs
@@ -0,0 +1,12 @@
+﻿using eShopOnContainers.Core.Models.Common;
+
+namespace eShopOnContainers.Core.Models.MyProduct
+{
+    public class ProductFilter : BaseEntity
+    {
+        public int? MainCategoryId { get; set; }
+        public int? SubCategoryId { get; set; }
+        public int? SubOfSubCategoryId { get; set; }
+        public string Title { get; set; }
+    }
+}
