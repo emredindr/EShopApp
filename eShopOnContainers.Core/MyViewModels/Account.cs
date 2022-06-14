44ff1ca9ff29e20ac7258fe69abe5e9675b44c48
emredindr <emredindr@gmail.com>
2022-06-14 10:42:05 +0300

    Account viewmodel oluşturuldu.

---
+++ eShopOnContainers.Core/MyViewModels/Account.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using eShopOnContainers.Core.Models.MyAccount;
+using eShopOnContainers.Core.Services.MyAccount;
+
+namespace eShopOnContainers.Core.MyViewModels
+{
+    public class Account
+    {
+        public List<AccountPageItem> AllAccountPageItems { get; set; }
+        public List<AccountPageItemLogined> AllAccountPageItemsLogined { get; set; }
+        public Account(IAccountManager accountManager)
+        {
+            AllAccountPageItems = accountManager.GetAccountPageItems();
+            AllAccountPageItemsLogined = accountManager.GetAccountPageItemsLogined();
+        }
+    }
+}
